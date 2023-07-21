a3022602db004d0ae4c2f07ba569f1a86516730e
Bruno Soares <brunofas12@gmail.com>
2023-07-21 13:29:29 +0800

    add some additional css style

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,27 @@ module.exports = {
         'netflixRedColor': '#E50914'
       },
       backgroundColor: {
-        'netRedColor': '#E50914'
+        'netRedColor': '#E50914',
+        'inputcolor': '#333333',
 
       },
       width: {
         '18': '4.4rem',
+        '22': '5.5rem'
       },
       height: {
         '18': '4.4rem',
+        '120': '36rem',
+
       },
       borderWidth: {
         DEFAULT: '1.5px',
         '0': '0',
-      }
+      },
+      backgroundImage: {
+        'home-netflix': "url('/src/Assets/netBackground.jpg')"
+
+      },
     },
   },
   plugins: [],
